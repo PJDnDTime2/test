a9c33e46b2c3245cb5c767709670f770f83b8a1d
PJDnDTime2 <44420187+PJDnDTime2@users.noreply.github.com>
2019-08-02 08:38:49 -0700

    Add files via upload

---
+++ navigation.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $(".jqMenu").click(function() {
+        var clickedID = "#" + $(this).attr('id') + "Menu";
+        if ($(clickedID + ":visible").length) {
+            $(clickedID).slideToggle();
+        } else if ($(".ddmenu:visible").length) {
+            $(".ddmenu:visible").slideUp(200, function() {
+                $(clickedID).slideToggle();
+				});
+		} else {
+			$(clickedID).slideToggle();
+        }
+    });
+});
+$(document).ready(function() {
+    $(".jqMMenu").click(function() {
+        var clickedID = "#" + $(this).attr('id') + "Menu";
+		$(clickedID).slideToggle();
+    });
+});
+$("a").click(function () {
+	if ($(this).hasClass("redtext")) {
+		$("a").removeClass("redtext");
+	} else {
+		$("a").removeClass("redtext");
+		$(this).toggleClass("redtext");
+	}
+});
